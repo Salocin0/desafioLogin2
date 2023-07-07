75bdf66e9fa4bdd70f96b70de941e48adf23c769
Salocin0 <64389452+Salocin0@users.noreply.github.com>
2023-07-07 19:09:14 -0300

    primer commit

---
+++ src/routes/login.router.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import { UserModel } from '../DAO/models/users.model.js';
+
+export const loginRouter = express.Router();
+
+loginRouter.post('/register', async (req, res) => {
+  const { firstName, lastName, age, email, password } = req.body;
+  if (!firstName || !lastName || !age || !email || !password) {
+    return res.status(400).render('error-page', { msg: 'faltan datos' });
+  }
+  try {
+    await UserModel.create({ firstName, lastName, age, email, password});
+    req.session.firstName = firstName;
+    req.session.email = email;
+    return res.redirect('/login');
+  } catch (e) {
+    console.log(e);
+    return res.status(400).render('error-page', { msg: 'controla tu email y intenta mas tarde' });
+  }
+});
+
+loginRouter.post('/login', async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res.status(400).render('error-page', { msg: 'faltan datos' });
+  }
+  if (email=="adminCoder@coder.com" && password=="adminCod3r123"){
+    res.clearCookie('userId')
+    res.cookie('userId', "admin", { maxAge: 3600000 });
+    return res.redirect('/vista/products');
+  }
+  try {
+    const foundUser = await UserModel.findOne({ email });
+    if (foundUser && foundUser.password === password) {
+      req.session.firstName = foundUser.firstName;
+      req.session.email = foundUser.email;
+      req.session.admin = foundUser.admin;
+      res.clearCookie('userId')
+      res.cookie('userId', foundUser._id, { maxAge: 3600000 });
+      return res.redirect('/vista/products');
+    } else {
+      return res.status(400).render('error-page', { msg: 'email o pass incorrectos' });
+    }
+  } catch (e) {
+    console.log(e);
+    return res.status(500).render('error-page', { msg: 'error inesperado en servidor' });
+  }
+});
+
---
+++ src/routes/products.vista.router.js
@@ -0,0 +1,58 @@
+import express from "express";
+import { productService } from "../services/products.service.js";
+import { UserModel } from "../DAO/models/users.model.js";
+
+export const routerVistaProducts = express.Router();
+
+routerVistaProducts.get("/", async (req, res) => {
+  const limit = req.query.limit || 3;
+  const page = req.query.page || 1;
+  const query = req.query.query;
+  const sort = req.query.sort;
+  const requestUrl = req.originalUrl;
+  const allProducts = await productService.getAllProducts(limit, page, query, sort);
+  const previusLink = await productService.getPrevLink(requestUrl, page, allProducts.hasPrevPage);
+  const postLink = await productService.getNextLink(requestUrl, page, allProducts.hasNextPage);
+  let user = null;
+  if(req.cookies.userId!=="admin"){
+    const userId = req.cookies.userId;
+    if (userId) {
+      user = await UserModel.findById(userId);
+      user.rol="Usuario"
+    }
+    
+  }else{
+    user = {
+      firstName:"admin",
+      lastName:"admin",
+      rol:"Admin",
+      email:"adminCoder@coder.com",
+    }
+  }
+  const foundUser = {
+    firstName:user.firstName,
+    lastName:user.lastName,
+    rol:user.rol,
+    email:user.email,
+  }
+ 
+  
+  res.status(200).render("products", {
+    p: allProducts.docs.map((product) => ({
+      name: product.title,
+      description: product.description,
+      price: product.price,
+      id: product._id
+    })),
+    pagingCounter: allProducts.pagingCounter,
+    page: allProducts.page,
+    totalPages: allProducts.totalPages,
+    hasPrevPage: allProducts.hasPrevPage,
+    hasNextPage: allProducts.hasNextPage,
+    prevPage: allProducts.prevPage,
+    nextPage: allProducts.nextPage,
+    prevLink: previusLink,
+    nextLink: postLink,
+    user: foundUser
+  });
+});
